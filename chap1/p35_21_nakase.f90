139772c829b0a921dc669e5a8b68e7a9ac58b1ec
shoken1011 <83270303+shoken1011@users.noreply.github.com>
2021-05-25 15:12:55 +0900

    Add files via upload

--- chap1/p35_21_nakase.f90
+++ chap1/p35_21_nakase.f90
@@ -2,15 +2,15 @@ program ensyu21
 implicit none
 integer :: i, n
 double precision :: theta,area,pi
-open(1, file = 'output1_21.d')
-pi=asin(1.0d+0)*2.0d+0
+open(1, file='output1_21.d')
+pi=2.0d0 * acos(0.0d0)
 write(* , *)' input n ( n > 3)'
 read(* , *) n
 if (n < 3) stop 'stop, n < 3'
-do i=3,n
-theta=2.0d+0*pi/real(i)
-area=real(i)/2.0d+0*sin(theta)
-write(1 , *) i,pi-area
-enddo
+do i = 3, n
+ theta = 2*pi / i
+ area = i * 0.5d0 * 1 * 1 * sin(theta)
+ write(1 , *) i, pi-area
+end do
 close(1)
 end program ensyu21
