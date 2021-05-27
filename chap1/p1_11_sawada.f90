897233fbe7d79c24146c980b6decc7fc044d68ab
yasutoshi-sawada <83383177+yasutoshi-sawada@users.noreply.github.com>
2021-05-27 12:50:57 +0900

    Add files via upload

--- chap1/p1_11_sawada.f90
+++ chap1/p1_11_sawada.f90
@@ -1,11 +1,8 @@
 program p1_11
  implicit none
- integer i,wa0,wa1
- wa0=0
- wa1=0
- do i=1,50
-  wa0=wa0+(2*i-1)
-  wa1=wa1+2*i
- end do
- write(*,*)'wa0, wa1, wa =',wa0, wa1, wa0+wa1
+ integer a,p,ans
+ write(*,*) 'Input a and p'
+ read(*,*) a,p
+ ans=a-int(a/p)*p
+ write(*,*) 'mod(a,p)=',ans
 end program p1_11
