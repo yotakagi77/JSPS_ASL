b98926e9601df492fe8c89537e788f6ccba834ef
shoken1011 <83270303+shoken1011@users.noreply.github.com>
2021-06-14 22:27:25 +0900

    Add files via upload

---
+++ chap4/p4_3_function.f90
@@ -0,0 +1,9 @@
+function rechar(c) result(rc)
+implicit none
+character(*), intent(in) :: c
+character(len(c)) rc
+integer i
+do i = 1, len(c)
+ rc(i : i) = c(len(c) - i + 1 : len(c) - i + 1)
+end do
+end function rechar
