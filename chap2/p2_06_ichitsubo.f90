513b2421f20994a18969a9c8d1bc28520223fdb0
ichitsubo <86281289+ichitsubo@users.noreply.github.com>
2022-05-13 00:56:21 +0900

    Update p2_06_ichitsubo.f90

--- chap2/p2_06_ichitsubo.f90
+++ chap2/p2_06_ichitsubo.f90
@@ -3,11 +3,20 @@ program name
     integer i
     real(kind(1d0)) u(3),cos_alpha,cos_beta,cos_gamma,norm
 
-    call random_number(u)
+    !call random_number(u)
+    write(*,*)"input u1 :"
+    read(*,*) u(1)
+    write(*,*)"input u2 :"
+    read(*,*) u(2)
+    write(*,*)"input u3 :"
+    read(*,*) u(3)
+    
     norm=0
 do i=1,3
     norm=norm+u(i)*u(i)
 end do
+
+if (norm==0.0d0) stop "stop"
 norm=Sqrt(norm)
 cos_alpha=u(1)/norm
 cos_beta=u(2)/norm
